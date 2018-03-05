b56085d36d02f633ec319ecd42971353ab7be8e2
rustam <rustam@kamberov.me>
2018-03-05 09:10:31 +0100

    controller clean-up

--- src/routes/discount_controller.php
+++ src/routes/discount_controller.php
@@ -16,17 +16,26 @@ $app->add(function ($req, $res, $next) {
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 });
 
+/**
+ * The endpoint is for testing
+ */
 $app->get('/api', function (Request $request, Response $response){
-    $data = array('this URL' => 'API endpoint', 'status' => 'success', 'version' => '1.0');
+    $data = array('this URL' => 'Test API endpoint', 'status' => 'success', 'version' => '1.0');
     return $response->withJson($data);
 
 });
-
+/**
+ * Main endpoint to access discounts
+ */
 $app->post('/api/discount/', function(Request $request, Response $response){
+    /* Order data passed to the service */
     $order = $request->getParsedBody();
+
+    /* Products data retrieved via another service */
     $url = "src/services/products.json";
     $productsArray = NetworkUtils::getData($url);
 
+    /* Main business logic class */
     $discount = new Discount($order, $productsArray);
 
     /* Defining conditions for total value discount*/
@@ -40,7 +49,5 @@ $app->post('/api/discount/', function(Request $request, Response $response){
     /* Create a response array */
     $responseArray = $discount->buildDiscountsArray();
     return $response->withJson($responseArray);
-    $getSumOfDiscounts =  $discount->getSumOfDiscounts();
-    $calcTotalDiscounted =  $discount->calcTotalDiscounted();
 
 });
